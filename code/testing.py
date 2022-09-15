4be1d84dbfdd4e223a7f2f8d2b3bf4080136fbba
motorfireman <motorfireman@gmail.com>
2022-09-15 23:38:59 +0800

    Update testing.py

--- code/testing.py
+++ code/testing.py
@@ -1,7 +1,1 @@
 from re import A
-import pandas as pd
-import numpy as np
-
-singapore = pd.read_csv(r'C:\Users\motor\OneDrive\Desktop\SIT Y1S1\dataset\covid19.csv')
-world = pd.read_csv(r'C:\Users\motor\OneDrive\Desktop\SIT Y1S1\dataset\owid-covid-data.csv')
-world
