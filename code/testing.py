e3e1cb19a975ac0cf9f2b60fe1acba171fced5ab
motorfireman <motorfireman@gmail.com>
2022-09-15 23:37:53 +0800

    Adding more dataset
    owid-covid-data file has really a lot of data from every country.

--- code/testing.py
+++ code/testing.py
@@ -1,3 +1,7 @@
 from re import A
 import pandas as pd
 import numpy as np
+
+singapore = pd.read_csv(r'C:\Users\motor\OneDrive\Desktop\SIT Y1S1\dataset\covid19.csv')
+world = pd.read_csv(r'C:\Users\motor\OneDrive\Desktop\SIT Y1S1\dataset\owid-covid-data.csv')
+world
