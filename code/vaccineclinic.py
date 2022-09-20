32f37e3f352d9d313557df94b8eb60cfabd7fa83
viviannzh <113587961+viviannzh@users.noreply.github.com>
2022-09-20 15:00:22 +0800

    Create vaccineclinic.py
    vaccine clinics sg code - viv

---
+++ code/vaccineclinic.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+clinics_df = pd.read_csv(
+    r"C:\Users\ngzhi\PycharmProjects\pythonProject\phpc-clinics-offering-covid-19-vaccine (1)\18th-august-2022.csv")
+
+# checking null value
+null = clinics_df.isna().sum()
+
+# type of vaccines provided in clinics sg
+types = clinics_df.vaccine
+clean = clinics_df.vaccine.drop_duplicates()
+
+# vaccine clinic locations for children
+child = clinics_df[clinics_df['vaccine'].str.contains('Children' or 'children')]
+
+# vaccine clinic locations for pfizer/comirnaty
+pfz = clinics_df[clinics_df['vaccine'].str.contains('Pfizer' or 'Comirnaty')]
+
+# vaccine clinic locations for sinovac
+snv = clinics_df[clinics_df['vaccine'].str.contains('Sinovac')]
+
+# vaccine clinic locations for novavax
+nova = clinics_df[clinics_df['vaccine'].str.contains('Novavax')]
+
+# asking users for input
+qns = int(input("This is the following datas we can provide for vaccines in singapore:"
+                "\n1) type of vaccines provided in singapore"
+                "\n2) type of vaccine clinic locations for children"
+                "\n3) type of vaccine clinic locations for pfizer/comirnaty"
+                "\n4) type of vaccine clinic locations for sinovac"
+                "\n5) type of vaccine clinic locations for novavax"
+                "\nPlease choose the number for the data you would like to find out more:"))
+if qns == 1:
+    print(clean)
+elif qns == 2:
+    print(child)
+elif qns == 3:
+    print(pfz)
+elif qns == 4:
+    print(snv)
+elif qns == 5:
+    print(nova)
+else:
+    print("Please enter a valid number")
