852b2cba28995cefc3c0dd47ebae542f11f6d7f9
motorfireman <motorfireman@gmail.com>
2022-09-23 00:21:22 +0800

    Calculated the % of people died from vaccination adverse reaction

--- code/Adverse Reaction.py
+++ code/Adverse Reaction.py
@@ -8,20 +8,14 @@ import pandas as pd                                                         #to 
 import numpy as np                                                         #for using data as array
 import matplotlib.pyplot as plt                                           #for plotting graph of x,y
 
-import sklearn
-from sklearn import datasets, linear_model                               #for model we want to predict by
-from sklearn.linear_model import LinearRegression                       #for model we want to predict by
-from sklearn.metrics import mean_squared_error, r2_score               #for mean error and variance calculation
-from sklearn.model_selection import train_test_split                  #spltting training and testing sets
-
 
 # In[2]:
 
 
 #retrieving and reading CSV using pandas library
 
-world = pd.read_csv(r'C:\Users\motor\OneDrive\Desktop\SIT Y1S1\dataset\adverse reaction\vaers_jan_nov_2021.csv')
-features = pd.read_csv(r'C:\Users\motor\OneDrive\Desktop\SIT Y1S1\dataset\adverse reaction\Features.csv')
+world = pd.read_csv(r'vaers_jan_nov_2021.csv')
+features = pd.read_csv(r'Features.csv')
 
 
 #displays up to 50 columns
@@ -33,55 +27,42 @@ pd.set_option('display.max_columns', 50)
 pd.set_option('display.max_rows', 200)
 
 
-# In[ ]:
-
-
-world.info()
-
-
 # In[3]:
 
 
-df2 = world.drop(world.columns[[0,1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,27,28,29,30,31,32,33,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]],axis = 1)
+features 
 
 
 # In[4]:
 
 
-count = df2.groupby(['VAX_DOSE_SERIES'])
-count['DIED'].value_counts()
+df2 = world
 
 
 # In[5]:
 
 
-count['RECOVD'].value_counts()
+# grouping people with different doses and count how many deaths in each dose due to vaccination.
+
+count = df2.groupby(['VAX_DOSE_SERIES'])
+count['DIED'].value_counts()
 
 
 # In[6]:
 
 
-#number of people with 1 dose and adverse reaction = 172336+152680+104516 = 429532
-#number of people with 2 dose and adverse reaction = 119572+89045+48383 = 257000
-#number of people with 3 dose and adverse reaction = 6217+5922+3556 = 15695
+# Sum of people with adverse reaction from each dose group by using RECOVD column.
+# RECOVED tell us how many people have recovered. 
 
-#percentage of people dying with 1 dose and adverse reaction = 1.6%
-#percentage of people dying with 2 dose and adverse reaction = 2.23%
-#percentage of people dying with 3 dose and adverse reaction = 1.04%
+count['RECOVD'].value_counts()
 
 
 # In[7]:
 
 
-# assign values to lists  
-data = [{'Number_of_people_with_adverse_reaction': 429532, '%_of_people_died_with_adverse_reaction': 1.6}, {'Number_of_people_with_adverse_reaction': 257000, '%_of_people_died_with_adverse_reaction': 2.23},{'Number_of_people_with_adverse_reaction':15695, '%_of_people_died_with_adverse_reaction': 1.04}]  
-  
-# Creates padas DataFrame by passing  
-# Lists of dictionaries and row index.  
-dframe = pd.DataFrame(data, index =['1st Dose', '2nd Dose', '3rd Dose'])  
-  
-# Print the dataframe  
-print(dframe)  
+#sum of people with adverse reaction, 1 dose = 172336+152680+104516 = 429532
+#sum of people with adverse reaction, 2 dose = 119572+89045+48383 = 257000
+#sum of people with adverse reaction, 3 dose = 6217+5922+3556 = 15695
 
 
 # In[8]:
@@ -106,7 +87,21 @@ df = pd.DataFrame({'Number_of_people_with_adverse_reaction': Number_of_people_wi
 ax = df.plot.bar(rot=0)
 
 
-# In[ ]:
+# In[10]:
+
+
+# assign values to lists  
+data = [{'Number_of_people_with_adverse_reaction': 429532, '%_of_people_died_with_adverse_reaction': 1.6}, {'Number_of_people_with_adverse_reaction': 257000, '%_of_people_died_with_adverse_reaction': 2.23},{'Number_of_people_with_adverse_reaction':15695, '%_of_people_died_with_adverse_reaction': 1.04}]  
+  
+# Creates padas DataFrame by passing  
+# Lists of dictionaries and row index.  
+dframe = pd.DataFrame(data, index =['1st Dose', '2nd Dose', '3rd Dose'])  
+  
+# Print the dataframe  
+print(dframe)  
+
+
+# In[11]:
 
 
 #The death rate is very low (1-2%) despite having symptons and adverse reaction from vaccionation (1 to 3 doses).
