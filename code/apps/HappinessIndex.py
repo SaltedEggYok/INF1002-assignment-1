f5b7faae2c6b5a676090590426482f73d974a042
SaltedEggYok <113581302+SaltedEggYok@users.noreply.github.com>
2022-10-21 09:40:44 +0800

    update happiness index

--- code/apps/HappinessIndex.py
+++ code/apps/HappinessIndex.py
@@ -108,10 +108,16 @@ ax18 = sns.barplot(x = "Country", y = "Score 2018", data = ct_df18, palette = "b
 for p in ax18.patches:
   ax18.text(p.get_x() + p.get_width()/2., p.get_height(), '{0:.3f}'.format(p.get_height()), fontsize = 12, color = 'maroon', ha = 'center', va = 'bottom')
 
-fig2018head = px.bar(data_frame=df18_head, x = 'Country', y= 'Score 2018',text = 'Score 2018', color = 'Country')
+fig2018head = px.bar(data_frame=df18_head, x = 'Country', y= 'Score 2018',text = 'Score 2018', 
+                    color = 'Country', title='Top 10 Happiest Countries in 2018',text_auto='.2f',
+                    labels={'Country':'Top Countries in 2018'})
 fig2018head.update_traces(textposition = 'outside')
-fig2018tail = px.bar(data_frame=df18_tail, x = 'Country', y= 'Score 2018',text = 'Score 2018', color = 'Country')
+fig2018head.update_yaxes(range=[0,10])
+fig2018tail = px.bar(data_frame=df18_tail, x = 'Country', y= 'Score 2018',text = 'Score 2018', 
+                    color = 'Country', title='Bottom 10 Happiest Countries in 2018',text_auto='.2f',
+                    labels={'Country':'Bottom Countries in 2018'})
 fig2018tail.update_traces(textposition = 'outside')
+fig2018tail.update_yaxes(range=[0,10])
 
 #WHR 2019
 df19_head = df19.head(10)
@@ -129,10 +135,16 @@ ax19 = sns.barplot(x = "Country", y = "Score 2019", data = ct_df19, palette = "b
 for p in ax19.patches:
   ax19.text(p.get_x() + p.get_width()/2., p.get_height(), '{0:.3f}'.format(p.get_height()), fontsize = 12, color = 'maroon', ha = 'center', va = 'bottom')
 
-fig2019head = px.bar(data_frame=df19_head, x = 'Country', y= 'Score 2019',text = 'Score 2019', color = 'Country')
+fig2019head = px.bar(data_frame=df19_head, x = 'Country', y= 'Score 2019',text = 'Score 2019', 
+                    color = 'Country', title='Top 10 Happiest Countries in 2019',text_auto='.2f',
+                    labels={'Country':'Top Countries in 2019'})
 fig2019head.update_traces(textposition = 'outside')
-fig2019tail = px.bar(data_frame=df19_tail, x = 'Country', y= 'Score 2019',text = 'Score 2019', color = 'Country')
+fig2019head.update_yaxes(range=[0,10])
+fig2019tail = px.bar(data_frame=df19_tail, x = 'Country', y= 'Score 2019',text = 'Score 2019',
+                    color = 'Country', title='Bottom 10 Happiest Countries in 2019',text_auto='.2f',
+                    labels={'Country':'Bottom Countries in 2019'})
 fig2019tail.update_traces(textposition = 'outside')
+fig2019tail.update_yaxes(range=[0,10])
 
 #WHR 2020
 df20_head = df20.head(10)
@@ -150,10 +162,16 @@ ax20 = sns.barplot(x = "Country", y = "Score 2020", data = ct_df20, palette = "b
 for p in ax20.patches:
   ax20.text(p.get_x() + p.get_width()/2., p.get_height(), '{0:.3f}'.format(p.get_height()), fontsize = 12, color = 'maroon', ha = 'center', va = 'bottom')
 
-fig2020head = px.bar(data_frame=df20_head, x = 'Country', y= 'Score 2020',text = 'Score 2020', color = 'Country')
+fig2020head = px.bar(data_frame=df20_head, x = 'Country', y= 'Score 2020',text = 'Score 2020', 
+                    color = 'Country', title='Top 10 Happiest Countries in 2020',text_auto='.2f',
+                    labels={'Country':'Top Countries in 2020'})
 fig2020head.update_traces(textposition = 'outside')
-fig2020tail = px.bar(data_frame=df20_tail, x = 'Country', y= 'Score 2020',text = 'Score 2020', color = 'Country')
+fig2020head.update_yaxes(range=[0,10])
+fig2020tail = px.bar(data_frame=df20_tail, x = 'Country', y= 'Score 2020',text = 'Score 2020', 
+                    color = 'Country', title='Bottom 10 Happiest Countries in 2020',text_auto='.2f',
+                    labels={'Country':'Bottom Countries in 2020'})
 fig2020tail.update_traces(textposition = 'outside')
+fig2020tail.update_yaxes(range=[0,10])
 
 #WHR 2021
 df21_head = df21.head(10)
@@ -171,10 +189,16 @@ ax21 = sns.barplot(x = "Country", y = "Score 2021", data = ct_df21, palette = "b
 for p in ax21.patches:
   ax21.text(p.get_x() + p.get_width()/2., p.get_height(), '{0:.3f}'.format(p.get_height()), fontsize = 12, color = 'maroon', ha = 'center', va = 'bottom')
 
-fig2021head = px.bar(data_frame=df21_head, x = 'Country', y= 'Score 2021',text = 'Score 2021', color = 'Country')
+fig2021head = px.bar(data_frame=df21_head, x = 'Country', y= 'Score 2021',text = 'Score 2021', 
+                    color = 'Country', title='Top 10 Happiest Countries in 2021',text_auto='.2f',
+                    labels={'Country':'Top Countries in 2021'})
 fig2021head.update_traces(textposition = 'outside')
-fig2021tail = px.bar(data_frame=df21_tail, x = 'Country', y= 'Score 2021',text = 'Score 2021', color = 'Country')
+fig2021head.update_yaxes(range=[0,10])
+fig2021tail = px.bar(data_frame=df21_tail, x = 'Country', y= 'Score 2021',text = 'Score 2021', 
+                    color = 'Country', title='Bottom 10 Happiest Countries in 2021',text_auto='.2f',
+                    labels={'Country':'Bottom Countries in 2021'})
 fig2021tail.update_traces(textposition = 'outside')
+fig2021tail.update_yaxes(range=[0,10])
 
 # FIND CORRELATION
 
@@ -186,7 +210,7 @@ plt.figure(figsize = (15,8))
 sns.set(font_scale=2.6)
 ax_corr19 = sns.heatmap(df19_rev.corr(), annot = True, vmin = -1, vmax = 1, center = 0,annot_kws={"size": 30}, cmap = 'YlGnBu')
 
-figHeatmap19 = px.imshow(df19_rev.corr())
+figHeatmap19 = px.imshow(df19_rev.corr(), title='2019 Happiness Heatmap',text_auto='.2f')
 #figHeatmap.show()
 
 # Merge df19 and df20 with dfCovid
@@ -213,8 +237,14 @@ df_WHR.head()
 sns.set(font_scale=1)
 df_WHR.plot(x = ['Score 2018', 'Score 2019', 'Score 2020', 'Score 2021'], title = 'Average World Happiness Score', ylabel = 'Score', kind = 'bar', color = 'aquamarine')
 
-figWHR = px.bar(data_frame=df_WHR, y=[df_WHR[0],df_WHR[1],df_WHR[2],df_WHR[3]], text=[df_WHR[0],df_WHR[1],df_WHR[2],df_WHR[3]])
-#figWHR.show()
+figWHR = px.bar(data_frame=df_WHR, 
+                y=[df_WHR[0],df_WHR[1],df_WHR[2],df_WHR[3]], 
+                text=[df_WHR[0],df_WHR[1],df_WHR[2],df_WHR[3]],
+                title='Average World Happiness Score',
+                text_auto='.3f',
+                labels={'index':'', 'y':'Average Happiness Score'})
+figWHR.update_traces(textposition = 'outside')
+figWHR.update_yaxes(range=[0,10])
 
 df19.head()
 
@@ -230,7 +260,7 @@ plt.figure(figsize = (15,8))
 sns.set(font_scale=2.6)
 ax_corr20 = sns.heatmap(df20_rev.corr(), annot = True, vmin = -1, vmax = 1, center = 0,annot_kws={"size": 30}, cmap = 'YlGnBu')
 
-figHeatmap20 = px.imshow(df20_rev.corr())
+figHeatmap20 = px.imshow(df20_rev.corr(), title='2020 Happiness Heatmap', text_auto='.2f')
 
 # SINGAPORE WHR DATA
 
@@ -243,37 +273,75 @@ layout = html.Div(children=[
     html.H1(children=[
         "Happiness Index"
     ]),
-    html.Div(children=[
-        dcc.Graph(figure = fig2018head),
-        dcc.Graph(figure = fig2018tail),
+    html.H4(children=['2018'],style={'textAlign':'center'}),
+    dbc.Row(children=[
+      dbc.Col(children=[
+        html.Div(children=[
+            dcc.Graph(figure = fig2018head),
+        ])
+      ],
+        width=6,
+      ),
+      dbc.Col(children=[
+        html.Div(children=[
+            dcc.Graph(figure = fig2018tail),
+        ])
+      ],
+      width=6,
+      ),      
     ]),
-    html.Div(children=[
-        dcc.Graph(figure = fig2019head),
-        dcc.Graph(figure = fig2019tail),
+    html.H4(children=['2019'],style={'textAlign':'center'}),
+    dbc.Row(children=[
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = fig2019head),
+        ]),
+      ),
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = fig2019tail),
+        ]),
+      ),      
     ]),
-    html.Div(children=[
-        dcc.Graph(figure = fig2020head),
-        dcc.Graph(figure = fig2020tail),
+    html.H4(children=['2020'],style={'textAlign':'center'}),
+    dbc.Row(children=[
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = fig2020head),
+        ]),
+      ),
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = fig2020tail),
+        ]),
+      ),      
     ]),
-    html.Div(children=[
-        dcc.Graph(figure = fig2021head),
-        dcc.Graph(figure = fig2021tail),
+    html.H4(children=['2021'],style={'textAlign':'center'}),
+    dbc.Row(children=[
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = fig2021head),
+        ]),
+      ),
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = fig2021tail),
+        ]),
+      ),      
     ]),
-    html.Div(children=[
-        dcc.Graph(figure = fig2018head),
-        dcc.Graph(figure = fig2018tail),
-    ]),
-    html.Div(children=[
-        dcc.Graph(figure = fig2018head),
-        dcc.Graph(figure = fig2018tail),
-    ]),
-    html.Div(children=[
-        dcc.Graph(figure = figHeatmap19),
+    dbc.Row(children=[
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = figHeatmap19),
+        ]),
+      ),
+      dbc.Col(
+        html.Div(children=[
+            dcc.Graph(figure = figHeatmap20),
+        ]),
+      ),      
     ]),
     html.Div(children=[
         dcc.Graph(figure = figWHR),
-    ]),
-    html.Div(children=[
-        dcc.Graph(figure = figHeatmap20),
     ]),
 ])
